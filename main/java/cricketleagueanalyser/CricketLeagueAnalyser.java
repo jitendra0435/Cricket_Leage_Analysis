a29c18b638868e56bf0937c8aa4228108eed0d37
jitendraB <jitendrabachhav0435@gmail.com>
2019-12-23 17:22:00 +0530

    Refactored Code by removing unnecessary code

--- main/java/cricketleagueanalyser/CricketLeagueAnalyser.java
+++ main/java/cricketleagueanalyser/CricketLeagueAnalyser.java
@@ -2,18 +2,16 @@ package cricketleagueanalyser;
 import com.google.gson.Gson;
 import java.util.*;
 import java.util.stream.Collectors;
-
 import static java.util.stream.Collectors.toCollection;
 
 public class CricketLeagueAnalyser {
     Map<String, IPLDAO> playerHashMap = new HashMap<>();
-    Map<String,IPLDAO>mapAllrounder=new HashMap<>();
+    Map<String,IPLDAO> mapAllrounder=null;
     Map<EnumField, Comparator<IPLDAO>> fieldNameComparatorMap = null;
     public enum Player {BATSMAN, BOWLER};
     private Player player;
-
     public CricketLeagueAnalyser() {
-
+        mapAllrounder=new HashMap<>();
         this.fieldNameComparatorMap = new HashMap();
         this.fieldNameComparatorMap.put(EnumField.ECONOMY, Comparator.comparing(census -> census.economy, Comparator.reverseOrder()));
         this.fieldNameComparatorMap.put(EnumField.STRIKERATES, Comparator.comparing(census -> census.batStrikeRate, Comparator.reverseOrder()));
@@ -23,19 +21,16 @@ public class CricketLeagueAnalyser {
         this.fieldNameComparatorMap.put(EnumField.STRIKERATESWITH4AND5WICKETS, new SortedMethodForWickets().reversed().thenComparing(cencus -> cencus.economy));
 
         Comparator<IPLDAO> averageComparator = Comparator.comparing(cencus -> cencus.average);
-        Comparator<IPLDAO> strikingRateComparator = Comparator.comparing(cencus -> cencus.batStrikeRate);
-        Comparator<IPLDAO> resultOfComparator = averageComparator.thenComparing(strikingRateComparator);
-        this.fieldNameComparatorMap.put(EnumField.StrikeRateWithAvg, resultOfComparator.reversed());
+        this.fieldNameComparatorMap.put(EnumField.StrikeRateWithAvg,averageComparator.thenComparing(cencus ->cencus.batStrikeRate).reversed());
 
         Comparator<IPLDAO> averageComparatorForRuns = Comparator.comparing(cencus -> cencus.average);
-        Comparator<IPLDAO> maxRuns = Comparator.comparing(cencus -> cencus.runs);
-        Comparator<IPLDAO> maxRunsWithAvg = averageComparatorForRuns.thenComparing(maxRuns);
-        this.fieldNameComparatorMap.put(EnumField.MaxRunsWithAvg, maxRuns.reversed());
+        this.fieldNameComparatorMap.put(EnumField.MaxRunsWithAvg, averageComparatorForRuns.thenComparing(cencus->cencus.runs));
 
-        Comparator<IPLDAO> maxWickets = Comparator.comparing(cencus -> cencus.wickets);
-        Comparator<IPLDAO> average = Comparator.comparing(cencus -> cencus.average);
-        Comparator<IPLDAO> mostWicketWithAvg = maxWickets.thenComparing(average);
-        this.fieldNameComparatorMap.put(EnumField.MAXWICKETSWITHAVERAGE,mostWicketWithAvg);
+        Comparator<IPLDAO> wicketComparator = Comparator.comparing(cencus -> cencus.wickets);
+        this.fieldNameComparatorMap.put(EnumField.MAXWICKETSWITHAVERAGE,wicketComparator.thenComparing(cencus -> cencus.average));
+
+        Comparator<IPLDAO> comparing4 = Comparator.comparing(census -> census.average);
+        this.fieldNameComparatorMap.put(EnumField.BESTAVGBAT_BALL, comparing4.thenComparing(census -> census.ballAverage).reversed());
     }
 
     public Map<String, IPLDAO> loadIPLCSV(Player player, String csvFilePath) throws CricketLeagueAnalyserException {
@@ -55,8 +50,7 @@ public class CricketLeagueAnalyser {
         return new Gson().toJson(arrayList);
     }
 
-    public List<IPLDAO> sortByAvg(Map<String, IPLDAO> batsManAvg, Map<String, IPLDAO> ballAvg, EnumField bestAvgbatBall) {
-
+    public List<IPLDAO> sortByAvg(Map<String, IPLDAO> batsManAvg, Map<String, IPLDAO> ballAvg, EnumField field) {
             for (Map.Entry<String, IPLDAO> entry : batsManAvg.entrySet()
             ) {
                 for (Map.Entry<String, IPLDAO> entry1 : ballAvg.entrySet()
@@ -68,7 +62,7 @@ public class CricketLeagueAnalyser {
                     }
             }
             List<IPLDAO> sortedPlayer = mapAllrounder.values().stream()
-                    .sorted(fieldNameComparatorMap.get(bestAvgbatBall).reversed())
+                    .sorted(fieldNameComparatorMap.get(field).reversed())
                     .collect(Collectors.toList());
             return sortedPlayer;
     }
